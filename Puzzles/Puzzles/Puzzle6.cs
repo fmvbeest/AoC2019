34ca07ac0667a60e0eda4daa9c25532bfecc44d0
martijn <fmvbeest@gmail.com>
2023-08-09 14:38:36 +0200

    dummy files Puzzle 6

---
+++ Puzzles/Puzzles/Puzzle6.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2019.Puzzles;
+
+public class Puzzle6 : PuzzleBase<IEnumerable<(string, string)>, int, int>
+{
+    protected override string Filename => "Input/puzzle-input-06.txt";
+    protected override string PuzzleTitle => "--- Day 6: Universal Orbit Map ---";
+    
+    public override int PartOne(IEnumerable<(string, string)> input)
+    {
+        return 0;
+    }
+
+    public override int PartTwo(IEnumerable<(string, string)> input)
+    {
+        return 0;
+    }
+
+    public override IEnumerable<(string, string)> Preprocess(IPuzzleInput input, int part = 1)
+    {
+        return input.GetAllLines().Select(item => item.Split(')')).Select(x => (x[0], x[1])).ToList();
+    }
+}
