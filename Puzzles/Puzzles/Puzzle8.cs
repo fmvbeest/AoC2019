5c246dd18b47c787f09d6fe3617edff3db871dba
martijn <fmvbeest@gmail.com>
2023-08-10 14:44:23 +0200

    Puzzle 8 dummy files

---
+++ Puzzles/Puzzles/Puzzle8.cs
@@ -0,0 +1,27 @@
+﻿using AoC2019.Util;
+using AoC2019.Util.Exceptions;
+using AoC2019.Util.Intcode.Enums;
+using AoC2019.Util.Intcode.Instructions;
+
+namespace AoC2019.Puzzles;
+
+public class Puzzle8 : PuzzleBase<IEnumerable<int>, int, int>
+{
+    protected override string Filename => "Input/puzzle-input-08.txt";
+    protected override string PuzzleTitle => "--- Day 8: Space Image Format ---";
+    
+    public override int PartOne(IEnumerable<int> input)
+    {
+        return 0;
+    }
+
+    public override int PartTwo(IEnumerable<int> input)
+    {
+        return 0;
+    }
+    
+    public override IEnumerable<int> Preprocess(IPuzzleInput input, int part = 1)
+    {
+        return input.GetFirstLine().Trim().Split(',').Select(int.Parse);
+    }
+}
