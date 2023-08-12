db48b3f5579b6bbba7f2837cc4cfc6af1d5d2c30
martijn <fmvbeest@gmail.com>
2023-08-12 16:05:22 +0200

    clenaup

--- TestPuzzles/TestPuzzle8.cs
+++ TestPuzzles/TestPuzzle8.cs
@@ -3,26 +3,13 @@ using AoC2019.Puzzles;
 
 public class TestPuzzle8
 {
-    private readonly PuzzleInput _testInput = new("Input/test-input-08.txt");
     private readonly PuzzleInput _puzzleInput = new("Input/puzzle-input-08.txt");
     private readonly Puzzle8 _puzzle = new();
 
     [Fact]
-    public void TestPartOneSample()
-    {
-        Assert.Equal(2, _puzzle.PartOne(_puzzle.Preprocess(_testInput)));
-    }
-    
-    [Fact]
     public void TestPartOneActual()
     {
         Assert.Equal(2413, _puzzle.PartOne(_puzzle.Preprocess(_puzzleInput)));
-    }
-    
-    [Fact]
-    public void TestPartTwoSample()
-    {
-        Assert.Equal(0, _puzzle.PartTwo(_puzzle.Preprocess(_testInput)));
     }
     
     [Fact]
