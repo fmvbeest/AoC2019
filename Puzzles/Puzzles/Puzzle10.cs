5d244ba9b57a5c6e0efef8dc426213edb069d851
martijn <fmvbeest@gmail.com>
2023-08-14 21:32:41 +0200

    Dummy files Puzzle 10

---
+++ Puzzles/Puzzles/Puzzle10.cs
@@ -0,0 +1,22 @@
+﻿namespace AoC2019.Puzzles;
+
+public class Puzzle10 : PuzzleBase<IEnumerable<string>, int, int>
+{
+    protected override string Filename => "Input/puzzle-input-10.txt";
+    protected override string PuzzleTitle => "--- Day 10: Monitoring Station ---";
+    
+    public override int PartOne(IEnumerable<string> input)
+    {
+        return 0;
+    }
+
+    public override int PartTwo(IEnumerable<string> input)
+    {
+        return 0;
+    }
+
+    public override IEnumerable<string> Preprocess(IPuzzleInput input, int part = 1)
+    {
+        return input.GetAllLines();
+    }
+}
---
+++ TestPuzzles/TestPuzzle10.cs
@@ -0,0 +1,33 @@
+namespace TestPuzzles;
+using AoC2019.Puzzles;
+
+public class TestPuzzle10
+{
+    private readonly PuzzleInput _testInput = new("Input/test-input-10.txt");
+    private readonly PuzzleInput _puzzleInput = new("Input/puzzle-input-10.txt");
+    private readonly Puzzle10 _puzzle = new();
+
+    [Fact]
+    public void TestPartOneSample()
+    {
+        Assert.Equal(0, _puzzle.PartOne(_puzzle.Preprocess(_testInput)));
+    }
+    
+    [Fact]
+    public void TestPartOneActual()
+    {
+        Assert.Equal(0, _puzzle.PartOne(_puzzle.Preprocess(_puzzleInput)));
+    }
+    
+    [Fact]
+    public void TestPartTwoSample()
+    {
+        Assert.Equal(0, _puzzle.PartTwo(_puzzle.Preprocess(_testInput)));
+    }
+    
+    [Fact]
+    public void TestPartTwoActual()
+    {
+        Assert.Equal(0, _puzzle.PartTwo(_puzzle.Preprocess(_puzzleInput)));
+    }
+}
